5703fd552a7a94ffca8b73cf6cbdcfba53aedb2a
George Suarez <georgesuarezdev@gmail.com>
2025-07-14 16:22:24 -0700

    Added JS file for particle effects

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// Loading particle effects
+window.onLoad = function () {
+  Particles.init({
+    selector: ".background",
+    color: ["#FAFAFA", "#FF0011"],
+    maxParticles: 250,
+    connectParticles: true,
+    responsive: [
+      {
+        breakpoint: 800,
+        options: {
+          color: "#FAF00DD",
+          maxParticles: 150,
+          connectParticles: false,
+        },
+      },
+    ],
+  });
+};
